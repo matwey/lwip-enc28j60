6a5038e39731802a6f2c5e0286471474e7595c12
chrysn <chrysn@fsfe.org>
2012-12-11 00:36:55 +0100

    split enc28j60 hardware-specific and general

---
+++ lwip-demo/enc28j60.c
@@ -0,0 +1,59 @@
+#include "enchw.h"
+#include "enc28j60.h"
+
+void enc_setup(void)
+{
+	enchw_setup();
+}
+
+static uint8_t enc_command(uint8_t first, uint8_t second)
+{
+	uint8_t result;
+	enchw_select();
+	enchw_exchangebyte(first);
+	result = enchw_exchangebyte(second);
+	enchw_unselect();
+	return result;
+}
+
+uint8_t enc_RCR(uint8_t reg) { return enc_command(reg & 0x1f, 0); }
+void enc_WCR(uint8_t reg, uint8_t data) { enc_command(0x40 | (reg & 0x1f), data); }
+void enc_BFS(uint8_t reg, uint8_t data) { enc_command(0x80 | (reg & 0x1f), data); }
+void enc_BFC(uint8_t reg, uint8_t data) { enc_command(0xa0 | (reg & 0x1f), data); }
+
+void enc_SC(void) { enchw_exchangebyte(0xff); }
+
+void enc_select_page(uint8_t page) { enc_WCR(ENC_ECON1, page & 0x03); }
+
+void enc_wait(void) { while (!(enc_RCR(ENC_ESTAT) & ENC_ESTAT_CLKRDY));}
+
+uint16_t enc_MII_read(uint8_t mireg)
+{
+	uint16_t result = 0;
+
+	enc_select_page(2);
+	enc_WCR(ENC_MIREGADR, mireg);
+	enc_BFS(ENC_MICMD, ENC_MICMD_MIIRD);
+
+	enc_select_page(3);
+	while(enc_RCR(ENC_MISTAT) & ENC_MISTAT_BUSY);
+
+	enc_select_page(2);
+	result = enc_RCR(ENC_MIRDL);
+	result |= enc_RCR(ENC_MIRDH)<<8;
+
+	enc_BFC(ENC_MICMD, ENC_MICMD_MIIRD);
+
+	return result;
+}
+
+void enc_MII_write(uint8_t mireg, uint16_t data)
+{
+	enc_select_page(3);
+	while(enc_RCR(ENC_MISTAT) & ENC_MISTAT_BUSY);
+
+	enc_select_page(2);
+	enc_WCR(ENC_MIREGADR, mireg);
+	enc_WCR(ENC_MIWRL, data & 0xff);
+	enc_WCR(ENC_MIWRH, data>>8);
+}
---
+++ lwip-demo/enc28j60.h
@@ -0,0 +1,46 @@
+/* all pages */
+#define ENC_ESTAT 0x1d
+#define ENC_ESTAT_CLKRDY 0x01
+#define ENC_ECON1 0x1f
+
+/* page 2 */
+#define ENC_MACON2 0x01
+#define ENC_MACON2_MARST 0x80
+
+#define ENC_MICMD 0x12
+#define ENC_MICMD_MIIRD 1
+#define ENC_MIREGADR 0x14
+#define ENC_MIWRL 0x16
+#define ENC_MIWRH 0x17
+#define ENC_MIRDL 0x18
+#define ENC_MIRDH 0x19
+
+/* page 3 */
+#define ENC_MISTAT 0x0a
+#define ENC_MISTAT_BUSY 1
+
+/* mii registers */
+#define ENC_PHLCON 0x14
+#define ENC_PHCON1 0x00
+#define ENC_PHCON2 0x01
+#define ENC_PHID1 0x02
+#define ENC_PHID2 0x03
+
+#define ENC_LCFG_ON 0x8
+#define ENC_LCFG_OFF 0x9
+#define ENC_LCFG_BLINKFAST 0xa
+#define ENC_LCFG_BLINKSLOW 0xb
+
+#define ENC_LACFG_OFFSET 8
+#define ENC_LBCFG_OFFSET 4
+
+void enc_setup(void);
+uint8_t enc_RCR(uint8_t reg);
+void enc_WCR(uint8_t reg, uint8_t data);
+void enc_BFS(uint8_t reg, uint8_t data);
+void enc_BFC(uint8_t reg, uint8_t data);
+void enc_SC(void);
+void enc_select_page(uint8_t page);
+void enc_wait(void);
+uint16_t enc_MII_read(uint8_t mireg);
+void enc_MII_write(uint8_t mireg, uint16_t data);
