c9bc9507bdcede023329d3f2a62b0d8eac301814
chrysn <chrysn@fsfe.org>
2012-12-10 17:12:55 +0100

    builds (but exceeds memory)

---
+++ lwip-demo/netif/mchdrv.c
@@ -0,0 +1,7 @@
+#include <netif/mchdrv.h>
+
+err_t mchdrv_init(struct netif *netif) {
+	return 0;
+}
+void mchdrv_poll(struct netif *netif) {
+}
---
+++ lwip-demo/netif/mchdrv.h
@@ -0,0 +1,10 @@
+#ifndef NETIF_MCHDRV_H
+#define NETIF_MCHDRV_H
+
+#include <lwip/netif.h>
+#include <lwip/err.h>
+
+err_t mchdrv_init(struct netif *netif);
+void mchdrv_poll(struct netif *netif);
+
+#endif
