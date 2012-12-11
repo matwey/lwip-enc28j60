4a64b6a213451cd1a3abda81288f25feb7237f3a
chrysn <chrysn@fsfe.org>
2012-12-11 02:51:03 +0100

    16-bit operations on enc28j60, self test started

--- lwip-demo/enc28j60.c
+++ lwip-demo/enc28j60.c
@@ -1,6 +1,9 @@
 #include "enchw.h"
 #include "enc28j60.h"
 
+/* Documentation references in here are relative to the ENC28J60 Data Sheet
+ * DS39662D */
+
 /** Stores the chip's active register page in order to avoid unnecessary
  * explicit changes. */
 static enc_register_t last_used_register;
@@ -10,6 +13,26 @@ void enc_setup(void)
 {
 	enchw_setup();
 	last_used_register = ENC_BANK_INDETERMINATE;
+}
+
+/** Run the built-in diagnostics. Returns 0 on success or an unspecified
+ * error code. */
+uint8_t enc_bist(void)
+{
+	/* according to 15.1 */
+	enc_WCR(ENC_EDMASTL, 0);
+	enc_WCR(ENC_EDMASTH, 0);
+	enc_WCR(ENC_EDMANDL, 0xff);
+	enc_WCR(ENC_EDMANDH, 0x1f);
+	enc_WCR(ENC_ERXNDL, 0xff);
+	enc_WCR(ENC_ERXNDH, 0x1f);
+	enc_BFS(ENC_ECON1, ENC_ECON1_CSUMEN);
+	/* skipping step 4 as we're using address fill mode */
+	enc_WCR(ENC_EBSTCON, ENC_EBSTCON_ADDRESSFILL | ENC_EBSTCON_TME | ENC_EBSTCON_BIST);
+	enc_BFS(ENC_ECON1, ENC_ECON1_DMAST);
+	while(!enc_RCR(ENC_ECON1 & ENC_ECON1_DMAST));
+
+	return 0;
 }
 
 static uint8_t command(uint8_t first, uint8_t second)
@@ -38,7 +61,16 @@ void enc_WCR(uint8_t reg, uint8_t data) { ensure_register_accessible(reg); comma
 void enc_BFS(uint8_t reg, uint8_t data) { ensure_register_accessible(reg); command(0x80 | (reg & 0x1f), data); }
 void enc_BFC(uint8_t reg, uint8_t data) { ensure_register_accessible(reg); command(0xa0 | (reg & 0x1f), data); }
 
-void enc_SC(void) { enchw_exchangebyte(0xff); }
+/** 16-bit register read. This only applies to ENC28J60 registers whose low
+ * byte is at an even offset and whose high byte is one above that. Can be
+ * passed either L or H sub-register. */
+/* FIXME: could use enc_register16_t */
+uint16_t enc_RCR16(enc_register_t reg) { return (enc_RCR(reg|1) << 8) | enc_RCR(reg&~1);}
+/** 16-bit register read. Compare enc_RCR16. Writes the lower byte first, then
+ * the higher. */
+void enc_WCR16(uint8_t reg, uint16_t data) { enc_WCR(reg&~1, data & 0xff); enc_WCR(reg|1, data >> 8); }
+
+void enc_SRC(void) { enchw_exchangebyte(0xff); }
 
 void enc_wait(void) { while (!(enc_RCR(ENC_ESTAT) & ENC_ESTAT_CLKRDY));}
 
@@ -51,8 +83,7 @@ uint16_t enc_MII_read(enc_register_t mireg)
 
 	while(enc_RCR(ENC_MISTAT) & ENC_MISTAT_BUSY);
 
-	result = enc_RCR(ENC_MIRDL);
-	result |= enc_RCR(ENC_MIRDH)<<8;
+	result = enc_RCR16(ENC_MIRDL);
 
 	enc_BFC(ENC_MICMD, ENC_MICMD_MIIRD);
 
@@ -64,6 +95,5 @@ void enc_MII_write(uint8_t mireg, uint16_t data)
 	while(enc_RCR(ENC_MISTAT) & ENC_MISTAT_BUSY);
 
 	enc_WCR(ENC_MIREGADR, mireg);
-	enc_WCR(ENC_MIWRL, data & 0xff);
-	enc_WCR(ENC_MIWRH, data>>8);
+	enc_WCR16(ENC_MIWRL, data);
 }
--- lwip-demo/enc28j60.h
+++ lwip-demo/enc28j60.h
@@ -1,11 +1,14 @@
 #include "enc28j60-consts.h"
 
 void enc_setup(void);
+uint8_t enc_bist(void);
 uint8_t enc_RCR(enc_register_t reg);
+uint16_t enc_RCR16(enc_register_t reg);
 void enc_WCR(uint8_t reg, uint8_t data);
+void enc_WCR16(uint8_t reg, uint16_t data);
 void enc_BFS(uint8_t reg, uint8_t data);
 void enc_BFC(uint8_t reg, uint8_t data);
-void enc_SC(void);
+void enc_SRC(void);
 void enc_wait(void);
 uint16_t enc_MII_read(enc_register_t mireg);
 void enc_MII_write(uint8_t mireg, uint16_t data);
