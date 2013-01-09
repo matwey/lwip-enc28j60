21da5388e9a87d34a9bedb0a8e933f2ad4b99b9c
chrysn <chrysn@fsfe.org>
2013-01-09 18:17:23 +0100

    can be pinged

--- lwip-demo/netif/mchdrv.c
+++ lwip-demo/netif/mchdrv.c
@@ -1,32 +1,48 @@
 #include <netif/mchdrv.h>
 #include <lwip/pbuf.h>
+#include <netif/etharp.h>
 #include "enc28j60.h"
 
 enc_operation_t encop;
+
+void mchdrv_poll(struct netif *netif) {
+	int receivedlength;
+	err_t result;
+	struct pbuf *buf = NULL;
+
+	if (enc_RCR(ENC_EPKTCNT)) {
+		enc_read_received_pbuf(&encop, &buf);
+		log_message("buffer was %u\n", (unsigned int)(buf));
+		result = netif->input(buf, netif);
+		log_message("received with result %d\n", result);
+	}
+}
+
+static err_t mchdrv_linkoutput(struct netif *netif, struct pbuf *p)
+{
+	log_message("Sending...");
+	enc_transmit_pbuf(&encop, p);
+	log_message("sent.\n");
+}
 
 err_t mchdrv_init(struct netif *netif) {
 	uint8_t mac_addr[6] = {0x02 /* u/l, local */, 0x04, 0xA3, /* microchip's oui as per manual is 00:04:a3 */
 		0x11, 0x22, 0x33};
 
+	log_message("Starting mchdrv_init.\n");
+
 	enc_setup();
 	enc_bist_manual();
 	enc_operation_setup(&encop, 4*1024, mac_addr);
-	return 0;
-}
-void mchdrv_poll(struct netif *netif) {
-	struct pbuf *buffer;
-	int receivedlength;
-	err_t result;
 
-	if (enc_RCR(ENC_EPKTCNT)) {
-		buffer = pbuf_alloc(PBUF_LINK, 100 /* i'll have to change the lower layer to fix that*/, PBUF_RAM);
-		if (buffer == NULL)
-		{
-			log_message("allocation failed\n");
-			return;
-		}
-		receivedlength = enc_read_received(&encop, buffer->payload, 100);
-		result = netif->input(buffer, netif);
-		log_message("received %d, result %d\n", receivedlength, result);
-	}
+	netif->state = &encop; /* is this how it's supposed to be used? */
+	netif->output = etharp_output;
+	netif->linkoutput = mchdrv_linkoutput;
+
+	netif->hwaddr_len = 6;
+	memcpy(netif->hwaddr, mac_addr, 6);
+
+	netif->flags |= NETIF_FLAG_ETHARP | NETIF_FLAG_LINK_UP;
+
+	return 0;
 }
