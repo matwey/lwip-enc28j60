d4aca52c76f83562829cbd9cd1befca18b13d915
chrysn <chrysn@fsfe.org>
2013-11-21 10:13:22 +0100

    update rtc documentation to match implementation

--- efm32/enchw/rtc.h
+++ efm32/enchw/rtc.h
@@ -1,19 +1,28 @@
 /** @file
  *
  * A simple real time clock interface that provides up to 64bit tick counts of
- * implementation-defined ticks.
+ * implementation-defined ticks, and a millisecond interface.
  *
- * @@@ obsolete
+ * This consists of hardware clock setup and maintenance (`rtc_setup`,
+ * `rtc_maintenance`), functions suitable to get the current number of system
+ * ticks (`rtc_get24`, `rtc_get32` and `rtc_get64`) and functions for getting
+ * milliseconds (`rtc_get_ms64`). Depending on the implementation, the lower
+ * the bit lengths, the faster the execution. (For example, a 24bit counter
+ * could be fetched from the hardware RTC, while the longer lengths might
+ * require locking).
  *
- * Apart from hardware clock setup (`rtc_setup`) and something suitable for
- * system ticks (`rtc_get`), this provides facilities for a 64bit high
- * precision clock that is usable for inside interrupts and does not depend on
- * having a highest-priority interrupt handler by itself. (@todo)
+ * If it is provided by the implementation, the `rtc_maintenance` function has
+ * to be called at least once after every clock wrap. All functions are safe to
+ * be called from interrupts too. (It is expected that there will be an
+ * additional implementation provided header file, which will allow inlining of
+ * functions or defining constant ticks per second later on; this will allow
+ * the user not to depend on knowledge of the implementation in order to
+ * determine whether or not `rtc_maintenance` has to be called.)
  *
  * No part of the ENC28J60 driver (EMLIB hardware implementation, ENC28J60
- * driver logic and lwIP port) currently use this, but it is provided for
- * convenience (and the lwIP port hooks the sys_now function up to it, even
- * though it is not used in the examples currently).
+ * driver logic and lwIP port) currently use this, but it is used in the
+ * example provided (along with simple code gluing `sys_ticks` to the RTC
+ * interface).
  *
  * It is expected that both the hardware implementation and the driver logic
  * will resort to using this for timeouts sooner or later.
