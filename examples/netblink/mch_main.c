f28d67b7441cb249dc9321cb67fd42b9d843f98f
chrysn <chrysn@fsfe.org>
2013-12-01 13:04:40 +0100

    make example work again after mac address moving

--- examples/netblink/mch_main.c
+++ examples/netblink/mch_main.c
@@ -10,6 +10,7 @@
 
 #include <rtc.h>
 #include <board.h>
+#include <enc28j60.h>
 
 #include <testapp.h>
 
@@ -18,13 +19,24 @@ struct ip_addr gw_addr = {0x0100a8c0UL}, netmask = {0x000000ffUL}; /* 192.168.0.
 
 static struct netif mchdrv_netif;
 
+static enc_device_t mchdrv_hw;
+
 void mch_net_init(void)
 {
     // Initialize LWIP
     lwip_init();
 
+    mchdrv_netif.hwaddr_len = 6;
+    /* demo mac address */
+    mchdrv_netif.hwaddr[0] = 0;
+    mchdrv_netif.hwaddr[1] = 1;
+    mchdrv_netif.hwaddr[2] = 2;
+    mchdrv_netif.hwaddr[3] = 3;
+    mchdrv_netif.hwaddr[4] = 4;
+    mchdrv_netif.hwaddr[5] = 5;
+
     // Add our netif to LWIP (netif_add calls our driver initialization function)
-    if (netif_add(&mchdrv_netif, &mch_myip_addr, &netmask, &gw_addr, NULL,
+    if (netif_add(&mchdrv_netif, &mch_myip_addr, &netmask, &gw_addr, &mchdrv_hw,
                 mchdrv_init, ethernet_input) == NULL) {
         LWIP_ASSERT("mch_net_init: netif_add (mchdrv_init) failed\n", 0);
     }
