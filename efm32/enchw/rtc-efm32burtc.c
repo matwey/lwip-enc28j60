c691b636fd722171c580ec994ac5bffe130d9393
chrysn <chrysn@fsfe.org>
2014-11-04 09:09:01 +0100

    outline burtc operation and limitations

--- efm32/enchw/rtc-efm32burtc.c
+++ efm32/enchw/rtc-efm32burtc.c
@@ -9,6 +9,28 @@
  * routine and can be queried inside interrupts.
  *
  * This implementation uses the BURTC clock available on some EFM32 devices.
+ *
+ * This implementation persists the tick count over poweroff situations using
+ * the backup capacitor, allows storing some data along with the backup-powered
+ * counter, and detects overflows.
+ *
+ * Overflows are currently not handles well: If the device is powered off duing
+ * an overflow (which happens at fixed points in time from the last resetting
+ * start of the device), it will indicate an overflow condition -- that's
+ * primarily because we guarantee that ticks increase monotonically modulo the
+ * bit length, and we can't rewind the counter to somewhere where we can count
+ * for long time after a poweroff.
+ *
+ * Possible future solutions are:
+ *
+ * * Use BURTC and RTC independently. BURTC can get a bigger prescaler (and
+ *   thus survive 194 days) and be reset at will.
+ * * Make a difference between the system-visible ticks and the BURTC ticks,
+ *   eg. rewind the BURTC once its MSB becomes 1, and store that state to
+ *   replay it to applications.
+ * * Use the BURTC's COMP0 interrupt flag instead of the overflow indicator.
+ *   COMP0 would, at runtime, be repeatedly set to be less than the count, and
+ *   only when the comparator interrupt flag is treated as an overflow.
  */
 
 #include "rtc.h"
